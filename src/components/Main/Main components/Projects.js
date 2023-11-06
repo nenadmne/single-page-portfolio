0d6cdc43aa05de5ad918b063e026c5e751ca2430
nenadmne <cosovicnenad14@gmail.com>
2023-11-06 15:24:05 +0100

    Project.CSS revritten on Tailwind framework

--- src/components/Main/Main components/Projects.js
+++ src/components/Main/Main components/Projects.js
@@ -6,75 +6,75 @@ import ImageOverlay from "../../Overlays/ImageOverlay";
 import "./Projects.css";
 
 const Projects = () => {
+  const projectsData = [
+    {
+      title: "Online-Store",
+      imgSrc: storeImg,
+      githubLink: "https://github.com/nenadmne/online-store",
+      websiteLink: "https://online-store-orcin-psi.vercel.app/",
+      technologies: ["React JS", "Node JS", "MySQL", "Responsive Design"],
+    },
+    {
+      title: "Meetups",
+      imgSrc: meetupImg,
+      githubLink: "https://github.com/nenadmne/meetups",
+      websiteLink: "https://meetups-five-phi.vercel.app/",
+      technologies: ["Next JS", "Responsive Design", "MongoDB"],
+    },
+    {
+      title: "Online-Library",
+      imgSrc: libraryImg,
+      githubLink: "https://github.com/nenadmne/Online-Library",
+      websiteLink: "https://online-biblioteka.netlify.app",
+      technologies: ["React JS", "Redux Toolkit", "Axios"],
+    },
+    {
+      title: "SilkRoad",
+      imgSrc: silkRoadImg,
+      githubLink: "https://github.com/nenadmne/coinis-online-store",
+      websiteLink: "https://silkroad-online-store.netlify.app",
+      technologies: ["React JS", "Context API", "MUI", "Responsive Design"],
+    },
+  ];
+
   return (
-    <section id="projects" className="projects">
-      <h3 class="text-2xl mb-4 sm:text-3xl sm:mb-12 lg:text-4xl lg:mb-16 !important">
+    <section id="projects" class="w-full sm:mb-20 lg:w-[1100px]">
+      <h3 class="text-2xl mb-4 sm:text-3xl sm:mb-12 lg:text-4xl lg:mb-16">
         Projects
       </h3>
       <div>
-        <ul>
-          <li>
-            <div className="image-container">
-              <h4> Online-Store</h4>
-              <img src={storeImg} />
-              <ImageOverlay
-                href="https://github.com/nenadmne/online-store"
-                link="https://online-store-orcin-psi.vercel.app/"
-              />
-            </div>
-            <p>
-              <span> React JS </span>
-              <span> Node JS </span>
-              <span> MySQL </span>
-              <span> Responsive Design </span>
-            </p>
-          </li>
-          <li>
-            <div className="image-container">
-              <h4> Meetups </h4>
-              <img src={meetupImg} />
-              <ImageOverlay
-                href="https://github.com/nenadmne/meetups"
-                link="https://meetups-five-phi.vercel.app/"
-              />
-            </div>
-            <p>
-              <span> Next JS </span>
-              <span> Responsive Design </span>
-              <span> MongoDB </span>
-            </p>
-          </li>
-          <li>
-            <div className="image-container">
-              <h4> Online-Library</h4>
-              <img src={libraryImg} />
-              <ImageOverlay
-                link="https://online-biblioteka.netlify.app"
-                href="https://github.com/nenadmne/Online-Library"
-              />
-            </div>
-            <p>
-              <span> React JS </span>
-              <span> Redux Toolkit </span>
-              <span> Axios </span>
-            </p>
-          </li>
-          <li>
-            <div className="image-container">
-              <h4> Online-Store</h4>
-              <img src={silkRoadImg} />
-              <ImageOverlay
-                link="https://silkroad-online-store.netlify.app"
-                href="https://github.com/nenadmne/coinis-online-store"
-              />
-            </div>
-            <p>
-              <span> React JS </span>
-              <span> Context API </span>
-              <span> MUI </span>
-              <span> Responsive Design </span>
-            </p>
-          </li>
+        <ul class="flex-nowrap flex-col w-full list-none p-0 m-0 flex gap-4 items-start justify-between sm:flex-wrap sm:flex-row sm:gap-8">
+          {projectsData.map((project, index) => (
+            <li
+              key={index}
+              class="w-full mb-4 border-b border-gray-300 border-t-1 flex flex-col justify-center lg:w-[47%] lg:mb-0 lg:border-0"
+            >
+              <div class="w-full relative cursor-pointer mb-4 transform transition-transform hover:scale-105 hover:opacity-100">
+                <h4 class="my-2 text-center sm:text-xl sm:my-4">
+                  {project.title}
+                </h4>
+                <img
+                  src={project.imgSrc}
+                  alt={project.title}
+                  class="w-[100%]"
+                />
+                <ImageOverlay
+                  href={project.githubLink}
+                  link={project.websiteLink}
+                />
+              </div>
+              <p class="mb-1 text-sm flex flex-row justify-center gap-4 sm:text-base sm:mb-0">
+                {project.technologies.map((tech, i) => (
+                  <span
+                    key={i}
+                    class="text-sm text-center uppercase font-light sm:text-base"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </p>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
